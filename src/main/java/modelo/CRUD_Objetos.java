20af17472b9b25b1065f502ff20cb7d7ea38747f
Pablo <Pablo@DESKTOP-1BDN3GD>
2022-12-06 01:26:29 +0100

    Parte usuarios completada

--- src/main/java/modelo/CRUD_Objetos.java
+++ src/main/java/modelo/CRUD_Objetos.java
@@ -16,4 +16,23 @@ public class CRUD_Objetos {
         List<Objetos> listaObjetos = session.createQuery("from Objetos ").getResultList();
         return listaObjetos;
     }
+    public static void reservar(int id){
+        SessionFactory factory = HibernateUtil.getSessionFactory();
+        Session session = HibernateUtil.getSession();
+        Objetos objReserved= (Objetos) session.createQuery("FROM Objetos  WHERE id_objeto ="+id).uniqueResult();
+        objReserved.setReservado(true);
+        session.beginTransaction();
+        session.update(objReserved);
+        session.getTransaction().commit();
+        session.close();
+    }
+    public static void insertarObjeto(Objetos obj){
+        SessionFactory factory = HibernateUtil.getSessionFactory();
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+        session.save(obj);
+
+        session.getTransaction().commit();
+        session.close();
+    }
 }
